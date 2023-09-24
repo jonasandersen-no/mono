b98357ab392e847f4a9b2539e56dc40eaf7ed158
Jonas Bjørge Andersen <jonas.bjorge.andersen@gmail.com>
2023-09-24 15:33:56 +0200

    Added some test configuration

--- src/main/java/com/bjoggis/mono/shared/configuration/SecurityConfiguration.java
+++ src/main/java/com/bjoggis/mono/shared/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.bjoggis.mono.shared.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
   @Bean
+  @Profile("!test")
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(authorize ->
             authorize.requestMatchers("/actuator/**").permitAll()
