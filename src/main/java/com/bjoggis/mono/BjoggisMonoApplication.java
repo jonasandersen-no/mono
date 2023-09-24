ce4ec20bace2322d1c6a62a68ed5c9a546006fcb
Jonas Bjørge Andersen <jonas.bjorge.andersen@gmail.com>
2023-09-24 15:03:20 +0200

    Initial commit

---
+++ src/main/java/com/bjoggis/mono/BjoggisMonoApplication.java
@@ -0,0 +1,13 @@
+package com.bjoggis.mono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjoggisMonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BjoggisMonoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bjoggis/mono/BjoggisMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjoggis.mono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BjoggisMonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
