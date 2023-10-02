d874e52d7012db9224ec2dba4bae5b9b26871eaa
Jonas Bjørge Andersen <dev@jonasandersen.no>
2023-10-02 20:52:27 +0200

    Added component annotation

--- src/main/java/com/bjoggis/mono/openai/adapter/out/openai/OpenAIAdapterImpl.java
+++ src/main/java/com/bjoggis/mono/openai/adapter/out/openai/OpenAIAdapterImpl.java
@@ -1,7 +1,9 @@
 package com.bjoggis.mono.openai.adapter.out.openai;
 
 import com.bjoggis.mono.openai.application.port.OpenAIAdapter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OpenAIAdapterImpl implements OpenAIAdapter {
 
   @Override
