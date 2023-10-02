3633e13ed6cd3ddbea72a3ba4a418a62d1b5aa0e
Jonas Bjørge Andersen <dev@jonasandersen.no>
2023-10-02 20:51:33 +0200

    Added adapter implementation

---
+++ src/main/java/com/bjoggis/mono/openai/adapter/out/openai/OpenAIAdapterImpl.java
@@ -0,0 +1,11 @@
+package com.bjoggis.mono.openai.adapter.out.openai;
+
+import com.bjoggis.mono.openai.application.port.OpenAIAdapter;
+
+public class OpenAIAdapterImpl implements OpenAIAdapter {
+
+  @Override
+  public String sendMessage(String message) {
+    return null;
+  }
+}
