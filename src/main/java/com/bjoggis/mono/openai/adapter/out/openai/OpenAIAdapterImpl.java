d6b9c0d31a61611257109e00e3c6e8d4f01d4bf0
Jonas Bjørge Andersen <dev@jonasandersen.no>
2023-10-02 21:07:12 +0200

    Added connection to openai and generate a message

--- src/main/java/com/bjoggis/mono/openai/adapter/out/openai/OpenAIAdapterImpl.java
+++ src/main/java/com/bjoggis/mono/openai/adapter/out/openai/OpenAIAdapterImpl.java
@@ -1,13 +1,41 @@
 package com.bjoggis.mono.openai.adapter.out.openai;
 
 import com.bjoggis.mono.openai.application.port.OpenAIAdapter;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatCompletionResult;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.service.OpenAiService;
+import java.time.Duration;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OpenAIAdapterImpl implements OpenAIAdapter {
 
+  private final OpenaiProperties properties;
+
+  private final String userId = UUID.randomUUID().toString();
+
+  public OpenAIAdapterImpl(OpenaiProperties properties) {
+    this.properties = properties;
+  }
+
   @Override
   public String sendMessage(String message) {
-    return null;
+    OpenAiService service = new OpenAiService(properties.token(), Duration.ofMinutes(1));
+
+    ChatCompletionRequest request = ChatCompletionRequest.builder()
+        .model("gpt-4")
+        .messages(List.of(new ChatMessage("user", message)))
+        .n(1)
+        .maxTokens(500)
+        .temperature(1.2)
+        .user(userId)
+        .build();
+
+    ChatCompletionResult response = service.createChatCompletion(request);
+
+    return response.getChoices().get(0).getMessage().getContent();
   }
 }
---
+++ src/test/java/com/bjoggis/mono/openai/adapter/out/openai/OpenAIAdapterImplTest.java
@@ -0,0 +1,7 @@
+package com.bjoggis.mono.openai.adapter.out.openai;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OpenAIAdapterImplTest {
+
+}
