b100d2e5ff056c950e41e0e97d684bd1efa9de2f
Jonas Bjørge Andersen <dev@jonasandersen.no>
2023-10-04 21:33:10 +0200

    Websocket sender test

---
+++ src/test/java/com/bjoggis/mono/openai/adapter/out/openai/WebSocketSenderImplTest.java
@@ -0,0 +1,25 @@
+package com.bjoggis.mono.openai.adapter.out.openai;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import com.bjoggis.mono.openai.adapter.in.websocket.ChatMessageResponse;
+import com.bjoggis.mono.openai.application.port.WebSocketSender;
+import org.junit.jupiter.api.Test;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+class WebSocketSenderImplTest {
+
+  @Test
+  void sendWillConvertAndSendToDestination() {
+    SimpMessagingTemplate simpMessagingTemplate = mock(SimpMessagingTemplate.class);
+
+    WebSocketSender webSocketSender = new WebSocketSenderImpl(simpMessagingTemplate);
+
+    webSocketSender.send(1L, "message");
+
+    verify(simpMessagingTemplate).convertAndSend("/topic/chat",
+        new ChatMessageResponse(1L, "message"));
+
+  }
+}
